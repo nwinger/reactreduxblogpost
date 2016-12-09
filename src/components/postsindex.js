398a163422756e70c40cfbf8c5d1d886bde9169b
Nicklas Winger <nwi@gecko.no>
2016-12-09 08:51:56 +0100

    Add PostsShow component and dynamic routing

--- src/components/postsindex.js
+++ src/components/postsindex.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
     componentWillMount() {
         this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        return _.map(this.props.posts, (post) => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    <Link to={"posts/" + post.id}>
+                        <div className="col-xs-6">
+                            <strong>{post.title}</strong>
+                        </div>
+                        <div className="col-xs-6 pull-xs-right">
+                            <span className="">[{post.categories}]</span>
+                        </div>
+                    </Link>
+                </li>
+            );
+        });
     }
 
     render() {
@@ -17,10 +35,19 @@ class PostsIndex extends Component {
                         Add a post
                     </Link>
                 </div>
-                List of blog posts
+                <h3>Posts</h3>
+                <ul className="list-group col-xs-12">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+    return {
+        posts: state.posts.all
+    };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
