89d2c56b4f3f0b67514d98bacc0629fe68ff2910
Nicklas Winger <nwi@gecko.no>
2016-12-09 07:40:12 +0100

    Add form validation and downgrade all packages to be equal to the course video. Created errors due to api changes

--- src/components/postsnew.js
+++ src/components/postsnew.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
     render() {
         const { fields: { title, categories, content }, handleSubmit } = this.props;
 
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={handleSubmit(this.props.createPost)}>
                 <h3>Create A New Post</h3>
                 <div className="form-group">
                     <label>Title</label>
                     <input type="text" className="form-control" {...title} />
+                    <div className="text-help">
+                        {title.touched ? title.error : ''}
+                    </div>
                 </div>
                 <div className="form-group">
                     <label>Categories</label>
@@ -18,7 +22,7 @@ class PostsNew extends Component {
                 </div>
                 <div className="form-group">
                     <label>Content</label>
-                    <textarea className="form-control" {...content}/>
+                    <textarea className="form-control" {...content} />
                 </div>
 
                 <button type="submit" className="btn btn-primary">Submit</button>
@@ -26,7 +30,22 @@ class PostsNew extends Component {
         );
     }
 }
+
+function validate(values) {
+    const errors = {};
+
+    if (!values.title) {
+        errors.title = 'Enter a username';
+    }
+
+    return errors;
+}
+
+// connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: first is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
 export default reduxForm({
     form: 'PostsNewForm',
-    fields: ['title', 'categories', 'content']
-})(PostsNew);
+    fields: ['title', 'categories', 'content'],
+    validate
+}, null, { createPost })(PostsNew);
