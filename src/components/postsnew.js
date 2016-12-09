307346368fcbb68e90731515572e387c5150cdaf
Nicklas Winger <nwi@gecko.no>
2016-12-09 08:27:49 +0100

    Navigate back to index when post successfully created

--- src/components/postsnew.js
+++ src/components/postsnew.js
@@ -1,31 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
+    onSubmit(props) {
+        this.props.createPost(props)
+            .then(() => {
+                // blog post has been created, navigate the user to the index
+                // We navigate by calling this.context.router.push with the
+                // new path to navigate to
+                this.context.router.push('/');
+            });
+    }
+
     render() {
         const { fields: { title, categories, content }, handleSubmit } = this.props;
 
         return (
-            <form onSubmit={handleSubmit(this.props.createPost)}>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <h3>Create A New Post</h3>
-                <div className="form-group">
+
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
                     <label>Title</label>
                     <input type="text" className="form-control" {...title} />
                     <div className="text-help">
                         {title.touched ? title.error : ''}
                     </div>
                 </div>
-                <div className="form-group">
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
                     <label>Categories</label>
                     <input type="text" className="form-control" {...categories} />
+                    <div className="text-help">
+                        {categories.touched ? categories.error : ''}
+                    </div>
                 </div>
-                <div className="form-group">
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
                     <label>Content</label>
                     <textarea className="form-control" {...content} />
+                    <div className="text-help">
+                        {content.touched ? content.error : ''}
+                    </div>
                 </div>
 
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -36,6 +59,14 @@ function validate(values) {
 
     if (!values.title) {
         errors.title = 'Enter a username';
+    }
+
+    if (!values.categories) {
+        errors.categories = 'Enter categories';
+    }
+
+    if (!values.content) {
+        errors.content = 'Enter some content';
     }
 
     return errors;
